dd503e3c750a1bc993ed2f2187ff2a0f405a4afa
Matthieu DESILE <matthieu.desile@gmail.com>
2011-11-28 14:57:37 +0100

    workaround when building openexr-1.7 with only x86_64 as arch

--- openexr17.sh
+++ openexr17.sh
@@ -57,7 +57,9 @@ mkdir -p "$REPOSITORYDIR/include";
 NATIVE_CXXFLAGS="-isysroot $NATIVE_SDK -arch $NATIVE_ARCH $NATIVE_OPTIMIZE \
 	-mmacos-version-min=$NATIVE_OSVERSION -D_THREAD_SAFE -O3 -dead_strip";
 
-NATIVE_ARCH=x86_64 # temporary 
+if [ $NUMARCH -eq 1 -a $ARCHS = "x86_64" ]; then
+    NATIVE_ARCH=x86_64 # workaround
+fi
 
 g++ -DHAVE_CONFIG_H -I./IlmImf -I./config \
     -I$REPOSITORYDIR/include/OpenEXR -D_THREAD_SAFE \
