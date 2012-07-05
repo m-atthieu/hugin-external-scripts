e75f2f6446ae9311b92832b39c093e724a5ccc32
Matthieu DESILE <matthieu.desile@gmail.com>
2012-07-05 11:27:20 +0200

    indentation

--- static-separation.sh
+++ static-separation.sh
@@ -1,13 +1,15 @@
 cd $REPOSITORYDIR
+
 if [ ! -d lib-static ] ; then
   mkdir -p lib-static
 fi
+
 if [ -n "lib/*.a" ] ; then
-  cp -f lib/*.a lib-static/
-  rm lib/*.a
+  cp -f lib/*.a lib-static/ && rm lib/*.a
 fi
+
 if [ ! -d lib-static/wx ] ; then
-  mv lib/{pkgconfig,wx} lib-static/
-  cp -a ../lib-static/pkgconfig lib/pkgconfig
-  cp -a ../lib-static/wx        lib/wx
+    cp -a lib/{pkgconfig,wx} lib-static/
+    #cp -a ../lib-static/pkgconfig lib/pkgconfig
+    #cp -a ../lib-static/wx        lib/wx
 fi
